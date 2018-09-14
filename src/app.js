97534cd85ec53eca9d9fdbd94b644288f50ab26f
doudou-djeck <mehdi.douah@epitech.eu>
2018-09-14 14:25:59 +0200

    J'ai ajouté un système de template pour le menu, écrit le contenue de chaque page

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import './sass/main.sass'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+
+import menu from './template/menu.html'
+
+$(document).bind('DOMContentLoaded', set_template)
+
+function set_template() {
+    let subtitle = $('#menu').html()
+    $('#menu').html(menu)
+    $('#menu-subtitle').html(subtitle)
+    $('#menu').addClass('row affix')
+}
