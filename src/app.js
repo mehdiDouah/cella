0575b8e0d9a83d33eace3b3590a7f5ab7769bf51
doudou-djeck <mehdi.douah@epitech.eu>
2018-09-15 18:11:09 +0200

    intégration de la photo, agencement de la map, première possibilité de rendu

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import $ from 'jquery'
-
+import { observeStickyEvents, unobserveStickyEvents, StickyEvent } from 'sticky-events'
 import menu from './template/menu.html'
 
-$(document).bind('DOMContentLoaded', set_template)
+$(document).bind('DOMContentLoaded', main)
 
-function set_template() {
+function main() {
     let subtitle = $('#menu').html()
     $('#menu').html(menu)
     $('#menu-subtitle').html(subtitle)
     $('#menu').addClass('row affix')
+    //$('#menu').addClass('row sticky-events affix')
 }
